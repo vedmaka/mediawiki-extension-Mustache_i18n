9adbe8a126e2724f09f79475f72692c2f2f7ff25
Vedmaka <god.vedmaka@gmail.com>
2016-08-09 15:55:05 +0300

    playing with Travis

---
+++ tests/phpunit/Mustache_i18nTest.php
@@ -0,0 +1,17 @@
+<?php
+
+class Mustache_i18nTest extends MediaWikiTestCase {
+
+	protected function setUp() {
+		parent::setUp();
+	}
+
+	protected function tearDown() {
+		parent::tearDown();
+	}
+
+	function testOne() {
+		$this->assertEquals( true, true );
+	}
+
+}
